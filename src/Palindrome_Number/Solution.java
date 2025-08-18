b9f746a5dbc85ca1647187f04e667f37deacc868
darilsobze <darilsobze13@icloud.com>
2025-08-18 07:28:46 +0200

    Palindrome_Solution

--- src/Palindrome_Number/Solution.java
+++ src/Palindrome_Number/Solution.java
@@ -1,4 +1,24 @@
 package Palindrome_Number;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static boolean isPalindrome(int x) {
+        int sum=0;
+        String numberInString= Integer.toString(x);
+        for(int i=0; i<numberInString.length() ; i++){
+            if(numberInString.charAt(i) == numberInString.charAt(numberInString.length()-1-i)){
+                sum++;
+            }
+        }
+        return sum == numberInString.length();
+    }
+
+    public static void main (String[] args){
+        System.out.print("Enter a number : ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        System.out.print(isPalindrome(input));
+
+    }
 }
